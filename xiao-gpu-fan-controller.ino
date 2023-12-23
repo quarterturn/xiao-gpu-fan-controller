233984bd67ba32c1fc7510e308d5ff0ec5fe0916
quarterturn <github@matrixwide.com>
2023-12-23 15:00:13 -0500

    added second fan

--- xiao-gpu-fan-controller.ino
+++ xiao-gpu-fan-controller.ino
@@ -3,7 +3,7 @@
 //
 // hardware:
 // Seeed XIAO
-// 
+//
 // (per temperature sensor)
 // 1x 10K NTC (1%)
 // 1x 10K 1%
@@ -43,15 +43,19 @@ const int ledPin = 13;
 // pins
 const int ntc_1_pin = 10;
 const int fan_1_pin = 1;
+const int ntc_2_pin = 9;
+const int fan_2_pin = 2;
 
 // minimum PWM
 const int base_pwm = 30;
 
 // temperatures
 float ntc_1_temp;
+float ntc_2_temp;
 
 // PWM percentages
 int fan_1_pwm;
+int fan_2_pwm;
 
 void setup(void) {
   Serial.begin(115200);
@@ -62,9 +66,12 @@ void setup(void) {
   // configure pins
   pinMode(fan_1_pin, OUTPUT);
   pinMode(ntc_1_pin, INPUT);
+  pinMode(fan_2_pin, OUTPUT);
+  pinMode(ntc_2_pin, INPUT);
 
   // start fans
-  set_pwm(50, fan_1_pin);
+  set_pwm(100, fan_1_pin);
+  set_pwm(100, fan_2_pin);
   delay(3000);
 }
 
@@ -80,7 +87,7 @@ float read_thermistor(int t_pin) {
     delay(10);
   }
   average = 0;
-  for (i = 0; i < NUM_SAMPLES; i++){
+  for (i = 0; i < NUM_SAMPLES; i++) {
     average += samples[i];
   }
   average /= NUM_SAMPLES;
@@ -111,6 +118,8 @@ void set_pwm(int pwm_percent, int pwm_pin) {
     pwm_percent = 100;
   pwm_ratio = ((100 - float(pwm_percent)) / 100.0);
   value = int(pwm_ratio * 1023);
+  Serial.print(F("PIN "));
+  Serial.println(pwm_pin);
   Serial.print(F("PWM %: "));
   Serial.println(pwm_percent);
   Serial.print(F("PWM value: "));
@@ -124,6 +133,9 @@ void loop(void) {
   ntc_1_temp = read_thermistor(ntc_1_pin);
   Serial.print(F("NTC 1 temp: "));
   Serial.println(ntc_1_temp);
+  ntc_2_temp = read_thermistor(ntc_2_pin);
+  Serial.print(F("NTC 2 temp: "));
+  Serial.println(ntc_2_temp);
   // the NTC reads about 10 C lower than the GPU temperature reported by nvidia-smi
   // anything below 32 C will be considered idle and use 30% PWM
   // anything above 42 C will be considered hot and use 100% PWM
@@ -131,7 +143,13 @@ void loop(void) {
     ntc_1_temp = 32;
   if (ntc_1_temp > 42)
     ntc_1_temp = 42;
+  if (ntc_2_temp < 32)
+    ntc_2_temp = 32;
+  if (ntc_2_temp > 42)
+    ntc_2_temp = 42;
   fan_1_pwm = map(ntc_1_temp, 32, 42, 30, 100);
   set_pwm(fan_1_pwm, fan_1_pin);
+  fan_2_pwm = map(ntc_2_temp, 32, 42, 30, 100);
+  set_pwm(fan_2_pwm, fan_2_pin);
   delay(500);
 }
